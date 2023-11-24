2d3f9e21046e642123355ed6dc6a6a5c1402cd9f
Artemijs Matusev <matu6ev@gmail.com>
2023-11-24 21:38:59 +0000

    Set up sign-in and registration routes, set up custom alert messages which can be sent to the front-end

--- index.py
+++ index.py
@@ -11,6 +11,11 @@ conn = sqlite3.connect(DB, check_same_thread=False)
 cursor = conn.cursor()
 
 
+#COLORS FOR THE ALERT MESSAGE
+green = "#149886"
+red = "#A45D5D"
+
+
 #Landing page, default endpoint
 @app.route('/')
 def index():
@@ -25,7 +30,7 @@ def login():
         #Get details from sign-in form
         username = request.form['username']
         password = request.form['password']
-        print(username, password)
+        #print(username, password)
 
         #Find user in DB
         storedDetails = getUserDetails(username)
@@ -33,15 +38,76 @@ def login():
         if storedDetails: #Check if the details exist
             storedUsername = storedDetails[0]#Get stored username and password
             storedPassword = storedDetails[1]
-            print(storedUsername, storedPassword)
-        else: #If no record found
-            print("Username doesn't exist")
+            #print(storedUsername, storedPassword)
 
-        return redirect(url_for('index'))
+            #Check stored hashed password with provided password
+            if bcrypt.check_password_hash(storedPassword, password):
+                print("Correct")
+            else:
+                return render_template('index.html', alertMessage="Sign-in Error: Invalid details", alertColor=red)
+                #print("Wrong password")
+
+        else: #If no record found (The tuple is empty)
+            #print("Username doesn't exist")
+            return render_template('index.html', alertMessage="Sign-in Error: Invalid details", alertColor=red)
+
     
+    #If HTTP Request method isn't POST
     else: # Else redirect to root
         return redirect(url_for('index'))
+    
+
         
+
+#Handle registration and redirect accordingly
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    #Check the HTTP request method
+    if request.method == 'POST':#If POST i.e. being accessed from html form
+        #Get details from the registration form
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        fullName = request.form['fullName']
+
+        #Hash the entered password
+        hashedPassword = bcrypt.generate_password_hash(password).decode('utf-8')
+
+        try: #Execute SQL query to INSERT new record to users table
+            cursor.execute(''' INSERT INTO users
+                           (username, email, password, full_name)
+                           VALUES (?, ?, ?, ?)''' , (username, email, hashedPassword, fullName,))
+            conn.commit() #Commit the changes to the SQL db
+            
+            return render_template('index.html', alertMessage="Account has been created", alertColor=green) #Render the index page again with alert message
+        
+        except sqlite3.Error as error: #Catch any errors
+
+            errorMessage = str(error) #Convert error to string
+
+            #Check error message to see which constraint has been failed
+            if (errorMessage == "UNIQUE constraint failed: users.email"):#EMAIL ALREADY EXISTS
+                return render_template('index.html', alertMessage="Error creating account: Email already in use", alertColor=red)
+            
+            elif (errorMessage == "UNIQUE constraint failed: users.username"):#USERNAME ALREADY EXISTS
+                return render_template('index.html', alertMessage="Error creating account: username already in use", alertColor=red)
+            
+            else:#OTHER ERRORS
+                print(error)
+                return render_template('index.html', alertMessage="Error creating account", alertColor=red)
+
+
+
+    else: #HTTP request method wasn't POST, redirect to index.
+        return redirect(url_for('index'))
+
+
+#The user dashboard
+@app.route('/dashboard')
+def dashboard():
+    return None
+
+
 
 
 
